8b2e906f7151ef5ddeb98e11a628586f85b509f5
Gianluca <gporta@gianlucaporta.it>
2024-07-12 16:33:53 +0200

    fix + aggiunta admin

---
+++ Observer/ConfigSaveObserver.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Threecommerce\Maintenance\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Threecommerce\Maintenance\Helper\Data;
+
+class ConfigSaveObserver implements ObserverInterface
+{
+    protected $helper;
+
+    public function __construct(
+        Data $helper
+    )
+    {
+        $this->helper = $helper;
+    }
+
+    public function execute(Observer $observer)
+    {
+        if ($this->getEnable() == 1)
+            $this->helper->addFlag();
+        else
+            $this->helper->removeFlag();
+    }
+}
